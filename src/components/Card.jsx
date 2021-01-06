af7d8f1afd870f00bd11a62fac2acd13f78d3eba
Samiul <samiulkarimprodhan@gmail.com>
2021-01-06 11:34:14 +0600

    Cpmtacts with React Properties

---
+++ src/components/Card.jsx
@@ -0,0 +1,18 @@
+import  React from "react";
+function Card(props) {
+   return <div className="card">
+    <div className="top">
+      <h2 className = "name">{props.name}</h2>
+      <img className = "circle-img"
+        src={props.image}
+        alt="avatar_img"
+      />
+    </div>
+    <div className="bottom">
+    <div className = "info"><p>{props.number}</p>
+      <p>{props.email}</p></div>
+      
+    </div>
+  </div>
+};
+export default Card;
